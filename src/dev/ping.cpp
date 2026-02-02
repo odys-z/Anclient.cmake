b5b7a0b1084644031c50167f5f9f02fb8c3bef7d
Odys Zhou <odys.zhou@gmail.com>
2026-02-02 11:14:40 +0800

    .

--- src/dev/ping.cpp
+++ src/dev/ping.cpp
@@ -1,5 +1,8 @@
 /**
  * The clangd helper
+ *
+ * Tip: If the are error in depending header files, e.g., jprotocol.h cannot include entt (Qt:F2 works),
+ * change something in jprotocol.h and save, recompile. This usually works.
  */
 #include <entt/meta/meta.hpp>
 #include <io/odysz/jprotocol.h>
